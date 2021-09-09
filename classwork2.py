c7b5d796c18e80f9a36eb68728c9828fd5e8526b
ZaidAG <72582743+ZaidAG@users.noreply.github.com>
2021-09-08 18:22:24 -0700

    Add files via upload

---
+++ classwork2.py
@@ -0,0 +1,24 @@
+import csv
+with open('class2.csv',newline='')as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+file_data.pop(0)
+total_marks=0
+total_entries=len(file_data)
+for marks in file_data:
+    total_marks+=float(marks[1])
+mean=total_marks/total_entries
+print("Mean(average)is->"+str(mean))
+import pandas as pd
+import plotly.express as px
+df=pd.read_csv("class2.csv")
+fig=px.scatter(df,x="Student Name", y="Marks")
+fig.update_layout(shape=[
+dict(
+type='line',
+y0=mean,y1=mean,
+x0=0,x1=total_entries
+)
+])
+fig.update_yaxes(rangemode="torezo")
+fig.show()
